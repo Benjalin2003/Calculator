2ce9d8d7c834321822a6d234fed2df17be07e26c
Cybersport <Cybersport@26kadr.internal>
2025-04-07 12:15:03 +0300

    first commit

---
+++ tests/test_calc.py
@@ -0,0 +1,32 @@
+from src.calc import Calculator
+
+calc = Calculator()
+
+def test_addition():
+    assert calc.sum(2,2) == 4
+    assert calc.sum(2, 2) == 5
+    assert calc.sum(2, 2) == 3
+
+def test_devide():
+    assert calc.divide(2,1) == 2
+    assert calc.divide(2, 1) == 1
+    assert calc.divide(2, 0) == 0
+    assert calc.divide(0, 1) == 0
+    assert calc.divide(1, 2) == 1.5
+    assert calc.divide(1, 2) == 1
+
+def test_multyply():
+    assert calc.multiply(2, 3) == 6
+    assert calc.multiply(2, 3) == 5
+    assert calc.multiply(2, 0) == 0
+    assert calc.multiply(2, 0) == 2
+    assert calc.multiply(0, 3) == 0
+    assert calc.multiply(0, 3) == 3
+
+def test_subtract():
+    assert calc.subtract(2, 1) == 1
+    assert calc.subtract(2, 1) == 2
+    assert calc.subtract(2, 4) == -2
+    assert calc.subtract(2, 4) == 0
+    assert calc.subtract(2, 1) == 3
+
